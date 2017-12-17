40d2ae4d76107c11b46e8049e07fec19658403a0
Manash97 <manashbharali79@gmail.com>
2017-12-17 16:11:05 +0530

    Deleted some plugin

--- script/script.js
+++ script/script.js
@@ -1,4 +1,3 @@
-	
 	window.onscroll = function() {scroll_height()};
 	function scroll_height() {
 	    if (document.body.scrollTop > 700 || document.documentElement.scrollTop > 700) {
@@ -7,10 +6,4 @@
 	         document.getElementById("nav").className = "navbar navbar-expand-md ";
 	    }
 	}
-
-	$(document).ready(function(){
-		$("#more").click(function(){
-			$("#show").fadeToggle();
-		});
-	});
 
